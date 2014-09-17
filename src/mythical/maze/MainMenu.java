ef51ef20f1809284f9b8782e9f5d3200f3ed54eb
100032528 <100032528@CLHS-325-29-S.students.ccisd.net>
2014-09-17 14:17:53 -0500

    Added Classes

---
+++ src/mythical/maze/MainMenu.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mythical.maze;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 100032528
+ */
+public class MainMenu extends JPanel {
+    
+}
