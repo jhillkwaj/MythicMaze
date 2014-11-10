cd299a65dc47e45c853ee5ad1da05f3f7a7fc80c
Richard <Richard@FutureDong>
2014-11-10 16:58:46 -0600

    New Test class

---
+++ src/mythical/maze/LShape.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mythical.maze;
+
+/**
+ *
+ * @author Richard
+ */
+public class LShape {
+    
+    private int xPos, yPos;
+    private boolean active;
+    
+    public LShape(int x, int y)
+    {
+        
+    
+    }
+    public void moveDown()
+    {
+        
+    }
+}
