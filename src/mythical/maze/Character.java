43a5a9bc3dc3a20a34f3ee42ec72708fdb20acf2
abvenkgoo <abvenkgoo@gmail.com>
2014-12-11 16:03:45 +0000

    Character.java edited online with Bitbucket

--- src/mythical/maze/Character.java
+++ src/mythical/maze/Character.java
@@ -15,27 +15,52 @@ public class Character {
     
     private int xPos,yPos;
     
+    /**
+    *
+    * @param x an integer for the x coordinate
+    * @param y an integer for the y coordinate
+    */
     public void setStart(int x, int y)
     {
         xPos = x;
         yPos = y;
     }
+    
+    /**
+    *
+    */
     public void draw(Graphics g,int gridSizeX, int gridSizeY, int offsetX)
     {
         //draw the character in the grid block
     }
+    
+    /**
+    *
+    */
     public int getX()
     {
         return xPos;
     }
+    
+    /**
+    *
+    */
     public int getY()
     {
         return yPos;
     }
+    
+    /**
+    * @param x an integer for the x coordinate
+    */
     public void setX(int x)
     {
         xPos = x;
     }
+    
+    /**
+    * @param y an integer for the y coordinate
+    */
     public void setY(int y)
     {
         yPos = y;
