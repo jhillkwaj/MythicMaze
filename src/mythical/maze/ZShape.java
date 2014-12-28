994b41043a341f6dc44d7d485e4ca2cf4e57ae08
abvenkgoo <abvenkgoo@gmail.com>
2014-12-28 00:08:13 +0000

    javadocs added

--- src/mythical/maze/ZShape.java
+++ src/mythical/maze/ZShape.java
@@ -11,6 +11,15 @@ package mythical.maze;
  */
 public class ZShape extends Shape {
     
+    /*
+    * Gets three integers x, y, l.
+    * Uses the integers x and y as the coordinate (x, y) as the beginning point for drawing the shape.
+    * Uses l as the integer to represent the level that the user is currently on.
+    * @param x an integer that represents the shape's x coordinate
+    * @param y an integer that represents the shape's y coordinate
+    * @param l an integer that represents the level the user is currently on
+    * @see Shape
+    */
     public ZShape(int x, int y, int l)
     {
         super(x,y,l);
